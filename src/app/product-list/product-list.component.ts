b00a5675795b8ac81da9097b4916a2ab3cbd8117
Rafał Aleksandrzak <rafal.aleksandrzak@geotik.com>
2018-10-06 11:10:00 +0200

    Added list of promo products

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -14,11 +14,16 @@ export class ProductListComponent implements OnInit {
   }
 
   products: IProduct[] = [
-    { name: "test1", price: 10 },
-    { name: "test2", price: 20 },
-    { name: "test3", price: 30 },
-    { name: "test4", price: 40 },
-    { name: "test5", price: 50 }
+    { name: "test1", price: 10, promo: false},
+    { name: "test2", price: 20, promo: false },
+    { name: "test3", price: 30, promo: true },
+    { name: "test4", price: 40, promo: true },
+    { name: "test5", price: 50, promo: false }
   ]
   
+  getPromoProducts() : IProduct[]{
+    return this.products.filter(function (pr) {
+      return pr.promo === true;
+    });
+  }
 }
